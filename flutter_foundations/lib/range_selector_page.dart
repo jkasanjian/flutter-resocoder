7bcd93c35904ea240470acde87ddb6bec86d2d46
Joshua Kasanjian <jkasanjian@gmail.com>
2021-08-11 23:11:52 -0700

    create input form UI

--- flutter_foundations/lib/range_selector_page.dart
+++ flutter_foundations/lib/range_selector_page.dart
@@ -16,7 +16,24 @@ class _RangeSelectorPageState extends State<RangeSelectorPage> {
         title: Text('Select Range'),
       ),
       body: Form(
-        child: Container(),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Minimum',
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: false,
+                  signed: true,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_forward),
